c996d7b3713eaa83ca3849bae55adb2cc7f3f93b
Willian Santa Rosa Rattis <willian.rattis@hotmail.com>
2025-07-28 17:30:08 -0300

    Update apis-onboarding.js
    Add Address API

--- js/apis-onboarding.js
+++ js/apis-onboarding.js
@@ -6,6 +6,7 @@ const apis = [
                 { nome: "account", url: "https://sit-account-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://sit-account-corp-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://sit-account2fa-api.casasbahia.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://sit-address-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://sit-auth-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://sit-captcha-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://sit-recovery-api.casasbahia.com.br/api/swagger/index.html" }
@@ -14,6 +15,7 @@ const apis = [
                 { nome: "account", url: "https://hlg-account-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://hlg-account-corp-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://hlg-account2fa-api.casasbahia.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://hlg-address-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://hlg-auth-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://hlg-captcha-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://hlg-recovery-api.casasbahia.com.br/api/swagger/index.html" }
@@ -22,6 +24,7 @@ const apis = [
                 { nome: "account", url: "https://account-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://account-corp-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://account2fa-api.casasbahia.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://address-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://auth-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://captcha-api.casasbahia.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://recovery-api.casasbahia.com.br/api/swagger/index.html" }
@@ -35,6 +38,7 @@ const apis = [
                 { nome: "account", url: "https://sit-account-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://sit-account-corp-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://sit-account2fa-api.pontofrio.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://sit-address-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://sit-auth-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://sit-captcha-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://sit-recovery-api.pontofrio.com.br/api/swagger/index.html" }
@@ -43,6 +47,7 @@ const apis = [
                 { nome: "account", url: "https://hlg-account-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://hlg-account-corp-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://hlg-account2fa-api.pontofrio.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://hlg-address-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://hlg-auth-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://hlg-captcha-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://hlg-recovery-api.pontofrio.com.br/api/swagger/index.html" }
@@ -51,6 +56,7 @@ const apis = [
                 { nome: "account", url: "https://account-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://account-corp-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://account2fa-api.pontofrio.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://address-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://auth-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://captcha-api.pontofrio.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://recovery-api.pontofrio.com.br/api/swagger/index.html" }
@@ -64,6 +70,7 @@ const apis = [
                 { nome: "account", url: "https://sit-account-api.extra.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://sit-account-corp-api.extra.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://sit-account2fa-api.extra.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://sit-address-api.extra.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://sit-auth-api.extra.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://sit-captcha-api.extra.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://sit-recovery-api.extra.com.br/api/swagger/index.html" }
@@ -72,6 +79,7 @@ const apis = [
                 { nome: "account", url: "https://hlg-account-api.extra.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://hlg-account-corp-api.extra.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://hlg-account2fa-api.extra.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://hlg-address-api.extra.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://hlg-auth-api.extra.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://hlg-captcha-api.extra.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://hlg-recovery-api.extra.com.br/api/swagger/index.html" }
@@ -80,6 +88,7 @@ const apis = [
                 { nome: "account", url: "https://account-api.extra.com.br/api/swagger/index.html" },
                 { nome: "account corp", url: "https://account-corp-api.extra.com.br/api/swagger/index.html" },
                 { nome: "account 2fa", url: "https://account2fa-api.extra.com.br/api/swagger/index.html" },
+                { nome: "address", url: "https://address-api.extra.com.br/api/swagger/index.html" },
                 { nome: "auth", url: "https://auth-api.extra.com.br/api/swagger/index.html" },
                 { nome: "captcha", url: "https://captcha-api.extra.com.br/api/swagger/index.html" },
                 { nome: "recovery", url: "https://recovery-api.extra.com.br/api/swagger/index.html" }
